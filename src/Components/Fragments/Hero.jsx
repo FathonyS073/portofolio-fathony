bf801bccd48bab277f3cf0b1c676a23410605970
unknown <fathony404@gmail.com>
2025-03-13 23:05:31 +0700

    Progress Website  Portfolio : Progress Hero Section Animation

--- src/Components/Fragments/Hero.jsx
+++ src/Components/Fragments/Hero.jsx
@@ -7,8 +7,10 @@ const Hero = () => {
   return (
     <div>
       <div className='bg-white max-w-[1200px] h-[80vh] mx-auto flex flex-col-reverse sm:flex-row justify-center align-center'>
-        <div className="text-8xl">
-          Text
+        <div className="bg-blue-300 flex-col my-auto mx-auto">
+          <p className='text-xl md:text-5xl sm:text-4xl font-bold text-gray-400'>Hello I'm Fathony</p>
+          <h1 className='text-2xl md:text-5xl sm:text-4xl font-bold md:py-6'>I'm a <TypeAnimation sequence={['Front-End Developer', 1000, 'Back-End Developer', 1000]} wrapper="h1" cursor={true} repeat={Infinity} /></h1>
+
         </div>
         <div className="my-auto">
           <img src={PictureHero} alt="HeroPicture" 
