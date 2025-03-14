a5a4e3878f137dda215934251a7e58d13d633c46
unknown <fathony404@gmail.com>
2025-03-14 15:19:22 +0700

    Progress Website  Portfolio : Progress Hero Section Done

--- src/Components/Fragments/Hero.jsx
+++ src/Components/Fragments/Hero.jsx
@@ -1,50 +1,71 @@
-import React from 'react';
-import PictureHero from '../../assets/Picture/Hero.png';
-import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from 'react-icons/ai';
-import { TypeAnimation } from 'react-type-animation';
+import React from "react";
+import PictureHero from "../../assets/Picture/Hero.png";
+import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <div>
-      <div className='text-white max-w-[1200px] h-[70vh] mx-auto flex flex-col-reverse sm:flex-row justify-center items-center left-0'>
+    <div className="  w-full flex justify-center py-5">
+      <div className=" text-white max-w-screen-xl w-full mx-auto flex flex-col-reverse md:flex-row items-center gap-10 px-6 md:px-12">
         {/* Bagian Kiri (Profil) */}
-        <div className=" rounded-lg ml-0  mx-auto justify-center">
-          <p className='text-xl md:text-2xl text-gray-400 py-1'>Hello! I'am</p>
+        <div className="flex-1 text-center md:text-left">
+          <p className="text-xl md:text-2xl text-gray-400 my-2">Hello! I'am</p>
           <h1 className="text-3xl md:text-5xl font-bold">Fathony</h1>
-          <p className='text-lg md:text-xl text-gray-400 py-2'>Currently Learning about</p>
-          {/* Kontainer untuk TypeAnimation dengan tinggi tetap */}
-          <h1 className='text-2xl md:text-5xl font-bold min-h-[60px]'>
-            <TypeAnimation 
+          <p className="text-lg md:text-xl text-gray-400 mt-2">
+            Currently Learning about
+          </p>
+          <h1 className="text-3xl md:text-5xl font-bold min-h-[60px] my-1 md:my-4 ">
+            <TypeAnimation
               sequence={[
-                'UI/UX Design', 1000, 
-                'Web Developer', 1000,
-                'Data Science', 1000,
-              ]} 
-              wrapper="span" // Mengubah dari <h1> ke <span> agar tidak membuat perubahan tata letak
-              cursor={true} 
-              repeat={Infinity} 
+                "UI/UX Design",
+                1000,
+                "Web Developer",
+                1000,
+                "Data Science",
+                1000,
+              ]}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
             />
           </h1>
-          <div className="text-5xl flex justify-start gap-5 my-2 ">
-            <a target='_blank' href="https://github.com/FathonyS073"><AiFillGithub/></a>
-            <a target='_blank' href="https://www.linkedin.com/in/syaennulloh/"><AiFillLinkedin/></a>
-            <a target='_blank' href="https://www.instagram.com/ui.syaennulloh_/">
-            <AiFillInstagram/>
+          <div className="text-3xl md:text-4xl flex justify-center md:justify-start gap-2 my-1 md:my-4">
+            <a target="_blank" href="https://github.com/FathonyS073">
+              <AiFillGithub />
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/syaennulloh/">
+              <AiFillLinkedin />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/ui.syaennulloh_/"
+            >
+              <AiFillInstagram />
+            </a>
+          </div>
+
+          <div className="flex justify-center md:justify-start mt-5">
+            <a
+              href="https://drive.google.com/file/d/1w3RkCUOMDXLu9tpYYkfBDrEUT-s_X9rY/view?usp=sharing"
+              target="_blank"
+              className="rounded-md bg-purple-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-purple-400 transition duration-300"
+            >
+              Download My Resume
             </a>
           </div>
         </div>
 
         {/* Bagian Kanan (Gambar) */}
-        <div className="my-auto">
-          <img 
-            src={PictureHero} 
-            alt="HeroPicture" 
-            className='w-[300px] sm:w-[500px] mx-auto h-auto rounded-full'
+        <div className="flex-1 flex justify-center">
+          <img
+            src={PictureHero}
+            alt="HeroPicture"
+            className="w-[250px] sm:w-[400px] md:w-[450px] rounded-full "
           />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
--- src/Components/Fragments/Navbar.jsx
+++ src/Components/Fragments/Navbar.jsx
@@ -1,59 +1,69 @@
-import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'; // Perbaiki nama ikon
-import Logo from '../../assets/Picture/Logo.png';
-import NavbarItem from '../Elements/NavbarItem';
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"; // Perbaiki nama ikon
+import Logo from "../../assets/Picture/Logo.png";
+import NavbarItem from "../Elements/NavbarItem";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-    const handleNav = () => {
-        setNav(!nav);
-    };
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
-    const menu = [
-        { id: 1, name: 'Home' },
-        { id: 2, name: 'About' },
-        { id: 3, name: 'Experience' },
-        { id: 4, name: 'Project' },
-        { id: 5, name: 'Contact' }
-    ];
+  const menu = [
+    { id: 1, name: "Home" },
+    { id: 2, name: "About" },
+    { id: 3, name: "Experience" },
+    { id: 4, name: "Project" },
+    { id: 5, name: "Contact" },
+  ];
 
-    return (
-        <div className=' left-0 z-50 top-0 py-7 mx-auto max-w-[1200px]'>
-            <div className="flex justify-between items-center px-6">
-                {/* Logo */}
-                <img src={Logo} className='w-[100px] md:w-[120px]' />
+  return (
+    <div className=" left-0 z-50 top-0 py-7 mx-auto max-w-[1200px]">
+      <div className="flex justify-between items-center px-6">
+        {/* Logo */}
+        <img src={Logo} className="w-[100px] md:w-[120px]" />
 
-                {/* Desktop Navbar */}
-                <div className="hidden md:flex gap-9 pr-4">
-                    {menu.map((item) => (
-                        <NavbarItem key={item.id} name={item.name} />
-                    ))}
-                </div>
-
-                {/* Mobile Burger Icon */}
-                <div onClick={handleNav} className='block md:hidden cursor-pointer text-white'>
-                    {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
-                </div>
-            </div>
-            {/* Mobile Menu */}
-            <div className={`fixed left-0 top-0 w-[60%] h-full bg-gray-900 text-white p-5 ease-in-out duration-300 ${nav ? 'translate-x-0' : '-translate-x-full'}`}>
-                <div className="flex justify-between items-center">
-                    <img src={Logo} className='w-[100px]' />
-                    <div onClick={handleNav} className='cursor-pointer'>
-                        <AiOutlineClose size={25} />
-                    </div>
-                </div>
-                <ul className='mt-6 space-y-4'>
-                    {menu.map((item) => (
-                        <li key={item.id} className='text-lg font-semibold hover:text-gray-400 transition'>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        {/* Desktop Navbar */}
+        <div className="hidden md:flex gap-9 pr-4">
+          {menu.map((item) => (
+            <NavbarItem key={item.id} name={item.name} />
+          ))}
         </div>
-    );
+
+        {/* Mobile Burger Icon */}
+        <div
+          onClick={handleNav}
+          className="block md:hidden cursor-pointer text-white"
+        >
+          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      <div
+        className={`fixed left-0 top-0 w-[60%] h-full bg-gray-900 text-white p-5 ease-in-out duration-300 ${
+          nav ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-between items-center">
+          <img src={Logo} className="w-[100px]" />
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+        <ul className="mt-6 space-y-4">
+          {menu.map((item) => (
+            <li
+              key={item.id}
+              className="text-lg font-semibold hover:text-gray-400 transition"
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
