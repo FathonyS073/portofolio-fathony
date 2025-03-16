87f659cc4ffef3120d88896adadbe61831e31dcd
unknown <fathony404@gmail.com>
2025-03-16 22:45:11 +0700

    Progress Website  Portfolio : Progress Project Secction

--- src/Components/Fragments/Project.jsx
+++ src/Components/Fragments/Project.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const Project = () => {
   return (
-    <div>
-      <h1 className="text-white">Hello World</h1>
+    <div id="project" className="bg-[#232325]">
+      <div className="text-white pt-10">
+        <div className="w-full lg:w-[60vw] mx-auto">
+          <h2 className="text-3xl  text-center">
+            My <span className="text-purple-400">Projects</span>
+          </h2>
+          <p className="text-[13px] md:text-[15px] text-center leading-6 p-10 md:p-5">
+            I have worked on 15+ projects in Web Development, UI/UX Design, and
+            Machine Learning, covering various industries and use cases. My
+            projects include website development, mobile UI/UX design, data
+            analysis dashboards, and AI-powered solutions. Each project is
+            crafted with a focus on user experience, functionality, and
+            innovation to solve real-world problems.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
